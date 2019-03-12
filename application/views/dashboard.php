ec3cdeb42f60d9a0d962ea48d33cb7b585082a22
ravi <ravip8527@gmail.com>
2019-03-13 01:19:19 +0530

    update
    update

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -12,14 +12,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	 href="<?php echo base_url('assets/bootstrap/css/bootstrap-responsive.min.css'); ?>"/>
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/theme.css'); ?>" />
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/images/icons/css/font-awesome.css'); ?>" />
-	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.comcss?family=Open+Sans:400italic,600italic,400,600"/>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/http://fonts.googleapis.comcss?family=Open+Sans:400italic,600italic,400,600');?>"/>
+	<link type="text/css" rel='stylesheet' href="<?php echo base_url('assets/http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600')?>"/>
 </head>
-<body><body>
+<body>
         <div class="navbar navbar-fixed-top">
             <div class="navbar-inner">
                 <div class="container">
                     <a class="btn btn-navbar" data-toggle="collapse" data-target=".navbar-inverse-collapse">
-                        <i class="icon-reorder shaded"></i></a><a class="brand" href="loginpage.php">TEACHER DIARY </a>
+                        <i class="icon-reorder shaded"></i></a><a class="brand" href="index.html">Teacher diary </a>
                     <div class="nav-collapse collapse navbar-inverse-collapse">
                         <ul class="nav nav-icons">
                             <li class="active"><a href="#"><i class="icon-envelope"></i></a></li>
@@ -33,19 +34,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                         </button>
                         </form>
                         <ul class="nav pull-right">
-                            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown
-                                <b class="caret"></b></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="#">Item No. 1</a></li>
-                                    <li><a href="#">Don't Click</a></li>
-                                    <li class="divider"></li>
-                                    <li class="nav-header">Example Header</li>
-                                    <li><a href="#">A Separated link</a></li>
-                                </ul>
-                            </li>
+
                             <li><a href="#">Support </a></li>
                             <li class="nav-user dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                <img src="<?php echo base_url('assets/images/user.png');?>" class="nav-avatar" />
+                                <img src="<?php echo base_url(),"assets/"?>images/user.png" class="nav-avatar" />
                                 <b class="caret"></b></a>
                                 <ul class="dropdown-menu">
                                     <li><a href="#">Your Profile</a></li>
@@ -62,26 +54,54 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             </div>
             <!-- /navbar-inner -->
         </div>
-    <div class="dropdown">
-  <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dropdown Example
-  <span class="caret"></span></button>
-  <ul class="dropdown-menu">
-    <li><a href="#">HTML</a></li>
-    <li><a href="#">CSS</a></li>
-    <li><a href="#">JavaScript</a></li>
-  </ul>
-</div> <div class="footer">
+        <!-- /navbar -->
+		 <div class="wrapper">
             <div class="container">
-                <b class="copyright">&copy; 2019 Dr. Nitin Deepak - amrapali.ac.in </b>All rights reserved.
-            </div>
-        </div>
+                <div class="row">
+                    <div class="span3">
+                        <div class="sidebar">
+                            <ul class="widget widget-menu unstyled">
+                                <li class="active"><a href="index.html"><i class="menu-icon icon-dashboard"></i>Dashboard
+                                </a></li>
+                                <li><a href="activity.html"><i class="menu-icon icon-bullhorn"></i>News Update</a>
+                                </li>
+                                <li><a href="message.html"><i class="menu-icon icon-inbox"></i>Notification <b class="label green pull-right"> 0</b> </a></li>
+                                <li><a href="task.html"><i class="menu-icon icon-tasks"></i>Tasks <b class="label orange pull-right">
+                                    19</b> </a></li>
+                            </ul>
+                            <!--/.widget-nav-->
+                            
+                            
+                            <ul class="widget widget-menu unstyled">
+                                <li><a href="#"><i class="menu-icon icon-table"></i> Online Attendance </a></li>
+                                <li><a href="#"><i class="menu-icon icon-book"></i> Lesson-Plans</a></li>
+                                <li><a href="#"><i class="menu-icon icon-paste"></i>Internal-Marks </a></li>
+                                <li><a href="#"><i class="menu-icon icon-bold"></i> Assignments</a></li>
+                                <li><a href="#"><i class="menu-icon icon-bar-chart"></i>Time-Charts </a></li>
+                            </ul>
+                            <!--/.widget-nav-->
+                            <ul class="widget widget-menu unstyled">
+                                <li><a class="collapsed" data-toggle="collapse" href="#togglePages"><i class="menu-icon icon-cog">
+                                </i><i class="icon-chevron-down pull-right"></i><i class="icon-chevron-up pull-right">
+                                </i>More Pages </a>
+                                    <ul id="togglePages" class="collapse unstyled">
+                                        <li><a href="other-login.html"><i class="icon-inbox"></i>Login </a></li>
+                                        <li><a href="other-user-profile.html"><i class="icon-inbox"></i>Profile </a></li>
+                                        <li><a href="other-user-listing.html"><i class="icon-inbox"></i>All Users </a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#"><i class="menu-icon icon-signout"></i>Logout </a></li>
+                            </ul>
+                        </div>
+                        <!--/.sidebar-->
+                    </div>
+        
         <script  type="text/javascript" src="<?php echo base_url('assets/js/jquery-1.9.1.min.js'); ?>" ></script>       <script type="text/javascript" src="<?php echo base_url('assets/js/jquery-ui-1.10.1.custom.min.js'); ?>"></script>
         <script type="text/javascript" src="<?php echo base_url('assets/bootstrap/js/bootstrap.min.js'); ?>" ></script>
         <script type="text/javascript" src="<?php echo base_url('assets/js/flot/jquery.flot.js'); ?>"></script>
         <script type="text/javascript" src="<?php echo base_url('assets/js/flot/jquery.flot.resize.js'); ?>"></script>
-
         <script type="text/javascript" src="<?php echo base_url('assets/js/datatables/jquery.dataTables.js'); ?>"></script>
-
         <script type="text/javascript" src="<?php echo base_url('assets/js/common.js'); ?>"></script>
       
-    </body> 
+
+</body>
---
+++ application/views/signup_page.php
@@ -0,0 +1,175 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>TEACHER DIARY</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css'); ?>"/>
+	<link rel="stylesheet" type="text/css"
+	 href="<?php echo base_url('assets/bootstrap/css/bootstrap-responsive.min.css'); ?>"/>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/theme.css'); ?>" />
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/images/icons/css/font-awesome.css'); ?>" />
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/http://fonts.googleapis.comcss?family=Open+Sans:400italic,600italic,400,600');?>"/>
+</head>
+<body>
+
+	<div class="navbar navbar-fixed-top">
+		<div class="navbar-inner">
+			<div class="container">
+				<a class="btn btn-navbar" data-toggle="collapse" data-target=".navbar-inverse-collapse">
+					<i class="icon-reorder shaded"></i>
+				</a>
+
+			  	<a class="brand" href="index.html">
+			  		Teacher diary
+			  	</a>
+
+			<div class="nav-collapse collapse navbar-inverse-collapse">
+				
+					<ul class="nav pull-right">
+
+						<li><a href="login.php">
+							Sign In
+						</a></li>
+
+						
+
+						<li><a href="#">
+							Help us...
+						</a></li>
+					</ul>
+				</div><!-- /.nav-collapse -->
+			</div>
+		</div><!-- /navbar-inner -->
+	</div><!-- /navbar -->
+
+
+
+	<div class="wrapper">
+		<div class="container">
+			<div class="row">
+				<div class="module module-login span4 offset4">
+					<form class="form-vertical">
+						<div class="module-head">
+							<h3>Sign Up</h3>
+						</div>
+						<div class="module-body">
+							<div class="control-group">
+								<div class="controls row-fluid">
+									<input class="span12" type="text" id="Username" placeholder="Username">
+								</div>
+							</div>
+							<div class="control-group">
+								<div class="controls row-fluid">
+									<input class="span12" type="email" id="email" placeholder="Email">
+								</div>
+							</div>
+					
+						</div>
+						<div class="module-body">
+							<div class="control-group">
+								<div class="controls row-fluid">
+									<input class="span12" type="text" id="first-name" placeholder="First-name">
+								</div>
+							</div>
+							<div class="control-group">
+								<div class="controls row-fluid">
+									<input class="span12" type="text" id="sir-name" placeholder="Sir-name">
+								</div>
+							</div>
+						</div>
+						<div class="module-body">
+							<div class="control-group">
+								<div class="controls row-fluid">
+									<input class="span12" type="Password" id="Password" placeholder="Password">
+								</div>
+							</div>
+							<div class="control-group">
+								<div class="controls row-fluid">
+									<input class="span12" type="password" id="RePassword" placeholder="Re-Password">
+								</div>
+							</div>
+						</div>
+						<div class="module-body">
+							<div class="control-group">
+								<div class="controls row-fluid">
+									<div class="controls">
+												<select tabindex="1" data-placeholder="Select question.." class="span12">
+													<option value="">Select question ..</option>
+													<option value="question 1">First question</option>
+													<option value="question 2">Second question</option>
+													<option value="question 3">Third question</option>
+													<option value="question 4">Fourth question</option>
+												</select>
+											</div>
+									</div>
+							</div>
+							<div class="control-group">
+								<div class="controls row-fluid">
+									<input class="span12" type="text" id="answer" placeholder="Answer">
+								</div>
+							</div>
+						</div>
+						<div class="module-body">
+							<div class="control-group">
+								<div class="controls row-fluid">
+									<input class="span12" type="date" id="dateofbirth" placeholder="Date-Of-Birth">
+								</div>
+							</div>
+							<div class="control-group">
+								<div class="controls row-fluid">
+									
+									<label class="radio inline" "span12" >
+													<input type="radio" name="Teacher" id="Teacher" value="Teacher" checked="">
+													Teacher
+												</label>
+									<label class="radio inline">
+													<input type="radio" name="Student" id="Student" value="Student">
+													Student
+												</label> 
+									<label class="radio inline">
+													<input type="radio" name="Parents" id="Parents" value="Parents">
+													Parents
+												</label>
+								</div>
+							</div>
+							<div>
+							<hr>							</div>
+						
+					
+						
+						<div class="module-foot">
+							<div class="control-group">
+								<div class="controls clearfix">
+									<button type="submit" class="btn btn-primary pull-right">Login</button>
+								
+								</div>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div><!--/.wrapper-->
+
+	<div class="footer">
+		<div class="container">
+			 
+
+			<b class="copyright">@ravi.com </b> All rights reserved.
+		</div>
+	</div>
+	<div class="footer">
+		<div class="container">
+			 
+
+			<b class="copyright">&copy; ravi.com </b> All rights reserved.
+		</div>
+	</div>
+	<script type="text/javascript" src="<?php echo base_url('assets/scripts/jquery-1.9.1.min.js'); ?>" ></script>
+	<script type="text/javascript" src="<?php echo base_url('assets/scripts/jquery-ui-1.10.1.custom.min.js'); ?>" ></script>
+	<script type="text/javascript" src="<?php echo base_url('assets/bootstrap/js/bootstrap.min.js'); ?>" ></script>
+</body>

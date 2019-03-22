a719d4ce4c6c7a27b6ab65d70624694ac37f7bc4
ravi <ravip8527@gmail.com>
2019-03-22 13:48:29 +0530

    upa

--- application/controllers/Lessonedit_controller.php
+++ application/controllers/Lessonedit_controller.php
@@ -1,10 +1,10 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Lesson_controller extends CI_Controller
+class Lessonedit_controller extends CI_Controller
 {
 	function index()
 	{
-		$this->load->view('lessonplan');
+		$this->load->view('lessonedit');
 	}
 }?>

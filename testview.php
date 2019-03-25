37a34dd4080c84cb9ba561d96f56d241dd2523c3
satyam199 <48346416+satyam199@users.noreply.github.com>
2019-03-25 09:53:16 +0530

    updates
    updates

---
+++ testview.php
@@ -0,0 +1,134 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>TEACHER DIARY</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css'); ?>"/>
+	<link rel="stylesheet" type="text/css"
+	 href="<?php echo base_url('assets/bootstrap/css/bootstrap-responsive.min.css'); ?>"/>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/theme.css'); ?>" />
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/images/icons/css/font-awesome.css'); ?>" />
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/http://fonts.googleapis.comcss?family=Open+Sans:400italic,600italic,400,600');?>"/>
+	<link type="text/css" rel='stylesheet' href="<?php echo base_url('assets/http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600')?>"/>
+</head>
+<body>
+	<div class="container">
+	<h1 align="center">CSA TIME TABLE</h1>
+	<h3 align="center">BCA 4 SEM,SECTION-A</h3>
+	<table class="table table-bordered">
+								  <thead>
+									<tr bgcolor="lightblue">
+									  <th>DAY / TIME</th>
+									  <th>9:00-9:50</th>
+									  <th>9:50-10:40</th>
+									  <th>10:40-11:30</th>
+									  <th>11:30-11:50</th>
+									  <th>11:50-12:40</th>
+									  <th>12:40-01-30</th>
+									  <th>01:30-02:00</th>
+									  <th>02:00-02:50</th>
+									  <th>02:50-03:40</th>
+									  <th>03:40-04:50</th>
+									</tr>
+								  </thead>
+								  <tbody>
+									<tr>
+									  <td>MON</td>
+									  <td>BCA 403</td>
+									  <td>BCA 401</td>
+									  <td>BCA 405</td>
+									  <td></td>
+									  <td>PDP</td>
+									  <td>BCA 404</td>
+									  <td></td>
+									  <td>BCA 402</td>
+									  <td>GD</td>
+									  <td>LIBRARY</td>
+									</tr>
+									<tr>
+									  <td>TUE</td>
+									  <td>BCA 403</td>
+									  <td>BCA 402</td>
+									  <td>BCA 404</td>
+									  <td></td>
+									  <td>BCA 401</td>
+									  <td>APTI</td>
+									  <td></td>
+									  <td>BCA 404</td>
+									  <td>SPORTS ACTIVITY</td>
+									  <td>LIBRARY</td>
+									</tr>
+									<tr>
+									  <td>WED</td>
+									  <td>BCA 404</td>
+									  <td>BCA 402</td>
+									  <td>BCA 403</td>
+									  <td></td>
+									  <td>PDP</td>
+									  <td>APTI</td>
+									  <td></td>
+									  <td>BCA 401</td>
+									  <td>GD-LAB</td>
+									  <td>LIBRARY</td>
+									</tr>
+									<tr>
+										<td>THU</td>
+										<td>BCA 403</td>
+										<td>BCA 401</td>
+										<td>BCA 405</td>
+										<td></td>
+										<td colspan="2"><center>BCA 406(UNIX LAB)</center></td>
+										
+										<td></td>
+										<td>BCA 402</td>
+										<td>ABL</td>
+										<td>LIBRARY</td>
+									</tr>
+									<tr>
+										<td>FRI</td>
+										<td>BCA 403</td>
+										<td>BCA 402</td>
+										<td>BCA 405</td>
+										<td></td>
+										<td colspan="2"><center>BCA 406(JAVA LAB)</center></td>
+										
+										<td></td>
+										<td>BCA 403</td>
+										<td>REMEDIAL</td>
+										<td>LIBRARY</td>
+									</tr>
+									<tr>
+										<td>SAT</td>
+										<td>BCA 405</td>
+										<td>BCA 402</td>
+										<td>BCA 404</td>
+										<td></td>
+										<td colspan="2"><center>BCA 406(JAVA LAB)</center></td>
+										
+										<td></td>
+										<td>REMEDIAL</td>
+										<td>SPORTS ACTIVITY</td>
+										<td>LIBRARY</td>
+									</tr>	
+								  </tbody>
+								</table>
+
+								<h5> BCA 401 &nbsp; &nbsp; &nbsp; &nbsp;    DIGITAL COMMUNICATION & NETWORKS &nbsp; &nbsp; &nbsp; &nbsp;      DR ASHISH KHARE</h5>
+
+								<h5> BCA 402 &nbsp; &nbsp; &nbsp; &nbsp;     NUMERICAL & STATISTICAL TECHNIQUE &nbsp; &nbsp; &nbsp; &nbsp;     DR JAVED ALAM</h5>
+								<h5> BCA 403 &nbsp; &nbsp; &nbsp; &nbsp;     UNIX & SHELL PROGRAMMING &nbsp; &nbsp; &nbsp; &nbsp;             MR HARAHVARDHAN PANT</h5>
+								<h5> BCA 404 &nbsp; &nbsp; &nbsp; &nbsp;     ENVIRONMENTAL SCIENCE &nbsp; &nbsp; &nbsp; &nbsp;                MS SUCHITA KHOLIYA</h5>
+							    <h5> BCA 405 &nbsp; &nbsp; &nbsp; &nbsp;     JAVA PROGRAMMING &nbsp;  &nbsp; &nbsp; &nbsp;                     MR ANKUSH JOSHI</h5>
+							    <h5> BCA 406 &nbsp; &nbsp; &nbsp; &nbsp;     LAB JAVA / UNIX PROGRAMMING &nbsp; &nbsp; &nbsp; &nbsp;           MR ANKUSH JOSHI / HARSHVARDHAN PANT</h5>
+							    <h5> PDP-BCA &nbsp; &nbsp; &nbsp; &nbsp;     PERSONALITY DEVELOPMENT PROGRAM &nbsp; &nbsp; &nbsp; &nbsp;      MS MANISHA RANA</h5>
+							    <h5> APTI-BCA &nbsp; &nbsp; &nbsp; &nbsp;    APTITUDE &nbsp; &nbsp; &nbsp; &nbsp;                             MR MANOJ PANDEY</h5>
+							    <h5> GD-BCA &nbsp; &nbsp; &nbsp; &nbsp;       GRAPHICE DESIGN &nbsp; &nbsp; &nbsp; &nbsp;                     MR H S JALAL</h5>
+							   </div> 
+							
+
+</body>
+</html>

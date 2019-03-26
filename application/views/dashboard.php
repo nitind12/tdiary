9f5a108e5eef4e11b0cec9a752b50344f133f2f6
preeti <kushwahapreeti999@gmail.com>
2019-03-26 12:18:33 +0530

    update

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -5,15 +5,9 @@
                 <?php $this->load->view('templates/sidebar'); ?>
             </div>
             <div class="span9">
-<<<<<<< HEAD
+
                 <!-- code here -->
-                 
-            </div>
-=======
-                <!-- code here -->helloo..
-                
-             </div>
->>>>>>> 3c14f7fc8852d615cea89c72ccdcf96f0939add2
+
         </div>
     </div>
 </div>

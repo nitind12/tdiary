8d0bbb7c3b97597fb3157c7164dc3df6016851ee
preeti <kushwahapreeti999@gmail.com>
2019-03-26 12:33:06 +0530

    update
    update

--- application/config/database.php
+++ application/config/database.php
@@ -78,7 +78,7 @@ $db['default'] = array(
 	'hostname' => 'localhost',
 	'username' => 'root',
 	'password' => '',
-	'database' => 'tdiary_database',
+	'database' => 't_diary',
 
 	
 	'dbdriver' => 'mysqli',

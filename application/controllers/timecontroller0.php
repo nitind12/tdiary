58fb064b026ab3cab9a7f09c44681ebb6fd80b6a
satyam199 <48346416+satyam199@users.noreply.github.com>
2019-03-29 10:58:16 +0530

    update
    update

--- application/controllers/timecontroller0.php
+++ application/controllers/timecontroller0.php
@@ -1,9 +1,9 @@
 <?php
 defined('BASEPATH') OR exit('NO direct script access allowed');
-class Timecontroller extends CI_Controller{
+class Timecontroller0 extends CI_Controller{
 public function index()
 {
- $data['page_'] = 'testview3';
+ $data['page_'] = 'formtestview';
  $data['title'] = "Timetable";
  $this->load->view('templates/header',$data);
  $this->load->view('dashboard',$data);

d14ef620d2255378b2d82bd1e7a63eb2cb32ceee
satyam199 <48346416+satyam199@users.noreply.github.com>
2019-05-06 12:22:58 +0530

    update
    update

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -14,7 +14,7 @@
 
         <script type="text/javascript" src="<?php echo base_url('assets/js/Raj.js');?>?version=4.8"></script>
 
-        <script type="text/javascript" src="<?php echo base_url('assets/js/satyam.js');?>?version=1.1"></script>
+        <script type="text/javascript" src="<?php echo base_url('assets/js/satyam.js');?>?version=1.0"></script>
         
         <script type="text/javascript" src="<?php echo base_url('assets/js/preeti.js');?>?version=7.0"></script>
 

b8a4290bb9d5a1f4eab29ec411d83e5ad0d33685
ravi <ravip8527@gmail.com>
2019-05-08 11:55:42 +0530

    upp

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,4 +1,4 @@
-       
+        
         <script type="text/javascript" src="<?php echo base_url('assets/js/jquery-1.9.1.min.js'); ?>" ></script>
 
         <script type="text/javascript" src="<?php echo base_url('assets/js/ravi.js');?>?version=6.5"></script>
@@ -8,7 +8,8 @@
         <script type="text/javascript" src="<?php echo base_url('assets/js/satyam.js');?>?version=1.3"></script>
         
         <script type="text/javascript" src="<?php echo base_url('assets/js/preeti.js');?>?version=7.1"></script>
-        <script type="text/javascript" src="<?php echo base_url('assets/js/loader1.js');?>"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+       
 
         <script type="text/javascript" src="<?php echo base_url('assets/js/jquery-ui-1.10.1.custom.min.js');?>"></script>
        	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>  
@@ -19,7 +20,6 @@
         <script type="text/javascript" src="<?php echo base_url('assets/js/common.js'); ?>"></script>
         <script type="text/javascript"src="<?php echo base_url('assets/js/flot/jquery.flot.pie.js');?>" ></script>
 
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>  
         <!--script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script-->   
 

98d48e368dde5045500fb926958c1e47aad14822
preeti <kushwahapreeti999@gmail.com>
2019-05-08 11:51:58 +0530

    update

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,17 +1,4 @@
        
-        <script type="text/javascript" src="<?php echo base_url('assets/js/jquery-ui-1.10.1.custom.min.js');?>"></script>
-       	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>  
-        <script type="text/javascript" src="<?php echo base_url('assets/js/flot/jquery.flot.js'); ?>"></script>
-
-        <script type="text/javascript" src="<?php echo base_url('assets/js/flot/jquery.flot.resize.js'); ?>"></script>
-        <script type="text/javascript" src="<?php echo base_url('assets/js/datatables/jquery.dataTables.js'); ?>"></script>
-        <script type="text/javascript" src="<?php echo base_url('assets/js/common.js'); ?>"></script>
-        <script type="text/javascript"src="<?php echo base_url('assets/js/flot/jquery.flot.pie.js');?>" ></script>
-
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>  
-        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>   
-
         <script type="text/javascript" src="<?php echo base_url('assets/js/jquery-1.9.1.min.js'); ?>" ></script>
 
         <script type="text/javascript" src="<?php echo base_url('assets/js/ravi.js');?>?version=6.0"></script>
@@ -23,5 +10,19 @@
         <script type="text/javascript" src="<?php echo base_url('assets/js/preeti.js');?>?version=7.1"></script>
         <script type="text/javascript" src="<?php echo base_url('assets/js/loader1.js');?>"></script>
 
+        <script type="text/javascript" src="<?php echo base_url('assets/js/jquery-ui-1.10.1.custom.min.js');?>"></script>
+       	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>  
+        <script type="text/javascript" src="<?php echo base_url('assets/js/flot/jquery.flot.js'); ?>"></script>
+
+        <script type="text/javascript" src="<?php echo base_url('assets/js/flot/jquery.flot.resize.js'); ?>"></script>
+        <script type="text/javascript" src="<?php echo base_url('assets/js/datatables/jquery.dataTables.js'); ?>"></script>
+        <script type="text/javascript" src="<?php echo base_url('assets/js/common.js'); ?>"></script>
+        <script type="text/javascript"src="<?php echo base_url('assets/js/flot/jquery.flot.pie.js');?>" ></script>
+
+        <!--script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script-->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>  
+        <!--script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script-->   
+
+       
 </body>
 </html>

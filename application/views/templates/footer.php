12bffb97ef6a180dabc55dd0cb2ee1493e347515
preeti <kushwahapreeti999@gmail.com>
2019-05-08 15:21:33 +0530

    update

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,24 +1,24 @@
+       <script type="text/javascript" src="<?php echo base_url('assets/js/jquery-1.9.1.min.js'); ?>" ></script>
+
+        <script type="text/javascript" src="<?php echo base_url('assets/js/ravi.js');?>?version=5.8"></script>
+
+        <script type="text/javascript" src="<?php echo base_url('assets/js/Raj.js');?>?version=4.8"></script>
+
+        <script type="text/javascript" src="<?php echo base_url('assets/js/satyam.js');?>?version=1.1"></script>
         
-        <script type="text/javascript" src="<?php echo base_url('assets/js/jquery-1.9.1.min.js'); ?>" ></script>
-
-        <script type="text/javascript" src="<?php echo base_url('assets/js/ravi.js');?>?version=6.5"></script>
-
-        <script type="text/javascript" src="<?php echo base_url('assets/js/Raj.js');?>?version=4.9"></script>
-
+        <script type="text/javascript" src="<?php echo base_url('assets/js/preeti.js');?>?version=7.0"></script>
+         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script type="text/javascript" src="<?php echo base_url('assets/js/jquery-ui-1.10.1.custom.min.js');?>"></script>
-
-        <script type="text/javascript" src="<?php echo base_url('assets/js/satyam.js');?>?version=1.3"></script>
-        
-        <script type="text/javascript" src="<?php echo base_url('assets/js/preeti.js');?>?version=7.1"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-       
        	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>  
         <script type="text/javascript" src="<?php echo base_url('assets/js/flot/jquery.flot.js'); ?>"></script>
-
         <script type="text/javascript" src="<?php echo base_url('assets/js/flot/jquery.flot.resize.js'); ?>"></script>
-    
-    <script type="text/javascript" src="<?php echo base_url('assets/js/datatables/jquery.dataTables.js'); ?>"></script>
+        <script type="text/javascript" src="<?php echo base_url('assets/js/datatables/jquery.dataTables.js'); ?>"></script>
+        <script type="text/javascript" src="<?php echo base_url('assets/js/common.js'); ?>?version=1.1"></script>
+        
+      
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>    
 
+        
 
 </body>
 </html>

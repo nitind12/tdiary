fa206a331836bea7a374c71cf38d0f41d89ab9b1
ravi <ravip8527@gmail.com>
2019-05-18 16:40:38 +0530

    yy

--- application/models/login_model.php
+++ application/models/login_model.php
@@ -21,18 +21,24 @@ class Login_model extends CI_Model {
 
             if($row->category==3)
             {
-                $this->db->select('a.*,b.session_id,b.session_class_id,b.student_id,c.semester_id');
-                $this->db->from('std_personal a');
-                 $this->db->join('section b', 'a.session_id=b.session_id');
-                $this->db->join('add_class c', 'b.session_class_id=c.add_class_id');
-             $query1=$this->db->get();
-                 $row =$query1->row();
+                $this->db->select('a.*');
+                $this->db->where('username',$this->session->userdata('user'));
+                $this->db->from('sign_up a');
+                $query3=$this->db->get();
+                $row = $query3->row();
+                $this->session->set_userdata('studentid', $row->student_id);
+                
+                $this->db->select('b.*');
+                $this->db->from('add_class b');
+                $this->db->where('a.student_id', $this->session->userdata('studentid'));
+                $this->db->join('section a ', 'a.session_class_id=b.add_class_id');
+                $query1=$this->db->get();
+                $row =$query1->row();
             $this->session->set_userdata('sin', $row->session_id);
             $this->session->set_userdata('cos', $row->course_id);
-
             $this->session->set_userdata('tan', $row->semester_id);
             $this->session->set_userdata('cot', $row->section_id);
-            
+          
             } 
             if($row->category==2)
             {

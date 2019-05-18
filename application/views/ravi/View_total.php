84bd895dc3ae769f656304e3ba37c45364e8ce17
preeti <kushwahapreeti999@gmail.com>
2019-05-18 21:48:24 +0530

    update

--- application/views/ravi/View_total.php
+++ application/views/ravi/View_total.php
@@ -15,42 +15,42 @@
 
  <div class="module-body">
 
-    
-      <table cellpadding="0" cellspacing="0" border="0" class="table table-bordered table-striped  display" width="100%">
-          <tr>
-            <tr>
-                
-                <td>
-                 <div class="control-group">
-                   <label class="control-label" for="basicinput">Date From </label>
-                      <div class="controls">
-                          <input type="date" name="d1" class="span2">
-                      </div>
-                    </div>
-                </td>
-                 <td>
-                 <div class="control-group">
-                   <label class="control-label" for="basicinput">Date Upto</label>
-                      <div class="controls">
-                          <input type="date" name="d2" class="span2">
-                      </div>
-                    </div>
-                </td>
-                </tr>  
+    <form name="frmtotalatten" id="frmtotalatten"  Method="POST" action="<?php echo site_url('main/student_report_daywise_controller');?>">
+          <table cellpadding="0" cellspacing="0" border="0" class="table table-bordered table-striped  display" width="100%">
+              <tr>
                 <tr>
-                   <td colspan="3">
-                  <div class="control-group">
-                      <div class="controls">
-                        <div style="float: right;"> 
-                        <input type="submit"class="btn btn-primary" value="Search" id="cmbAttendanceReport">
+                    
+                    <td>
+                     <div class="control-group">
+                       <label class="control-label" for="basicinput">Date From </label>
+                          <div class="controls">
+                              <input type="date" name="d1" class="span2">
+                          </div>
+                        </div>
+                    </td>
+                     <td>
+                     <div class="control-group">
+                       <label class="control-label" for="basicinput">Date Upto</label>
+                          <div class="controls">
+                              <input type="date" name="d2" class="span2">
+                          </div>
+                        </div>
+                    </td>
+                    </tr>  
+                    <tr>
+                       <td colspan="3">
+                      <div class="control-group">
+                          <div class="controls">
+                            <div style="float: right;"> 
+                            <input type="submit"class="btn btn-primary" value="Search" id="Totalattensubmit">
+                          </div>
+                        </div>
                       </div>
-                    </div>
-                  </div>
-                  </td>
-                
-                </tr>
-               </tbody>
-           </table>
+                      </td>
+                    
+                    </tr>
+                   </tbody>
+               </table>
   
         <table class="table table-bordered table-striped" id="reportshere" name="reportshere">
             </table>

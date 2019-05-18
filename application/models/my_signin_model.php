51d3a08af81b96ca627e45b027b394ea62beac76
ravi <ravip8527@gmail.com>
2019-05-18 17:05:19 +0530

    up

--- application/models/my_signin_model.php
+++ application/models/my_signin_model.php
@@ -18,17 +18,13 @@ class My_signin_model extends CI_Model
 	function register()
 	{
 		$username_ = $this->input->post('txtUsername');
-		$email_ = $this->input->post('txtEmail');
-		$first_name = $this->input->post('txtfirst_name');
-		$sir_name = $this->input->post('txtsir_name');
 		$pwd = $this->input->post('txtPwd');
-		$phone = $this->input->post('Phone');
+		$student_id = $this->input->post('txtstudentid');
 		$question = $this->input->post('txtquestion');
 		$answer = $this->input->post('txtanswer');
 		$dateofbirth= $this->input->post('dateofbirth');
 		$cat = $this->input->post('rod');
 		$this->db->where('username', $username_);
-		$this->db->or_where('email', $email_);
 		$query = $this->db->get('sign_up');
 		
 		if($query->num_rows()!=0)
@@ -39,15 +35,11 @@ class My_signin_model extends CI_Model
 		{
 			$data = array
 			(
-				'Username'=>$username_,
-				'first_name'=>$first_name,	
-				'last_name'=>$sir_name,
-				'email'=>$email_,	
+				'username'=>$username_,
 				'password'=>$pwd,
-				'phone_no'=>$phone,
 				'question'=>$question,	
 				'answer'=>$answer,	
-				'date_of_birth'=>$dateofbirth,
+				'student_id'=>$student_id,
 				'category'=>$cat
 				
 			);

9eafb754a0d08bd10ba8034e4c5f9234250f6373
ravi <ravip8527@gmail.com>
2019-05-20 19:34:29 +0530

    Update Check_assign_report_model.php

--- application/models/Check_assign_report_model.php
+++ application/models/Check_assign_report_model.php
@@ -1,10 +1,11 @@
 <?php
 class Check_assign_report_model extends CI_Model{
+	
 	function assignment_report()
 	{
 		$intel = array();
 		$Subject = $this->input->post('Subject');
-		$assignment_id = $this->input->post('assignment_no');
+		$assignment_id =$this->input->post('assignment_no');
 		
 		$this->db->distinct('a.add_class_id');
 		$this->db->select('a.*,b.roll_no,b.assignment_id,b.assignment_status,b.checker_date,b.Grade,c.first_name');
@@ -13,8 +14,9 @@ class Check_assign_report_model extends CI_Model{
 		$this->db->where('a.semester_id',$this->session->userdata('tan'));
 		$this->db->where('a.section_id',$this->session->userdata('cot'));
 		$this->db->where('b.subject_id', $Subject);
-		$this->db->where('b.roll_no',$this->session->userdata('studentid'));
 		$this->db->where('b.assignment_id', $assignment_id);
+		
+		$this->db->where('b.roll_no',$this->session->userdata('studentid'));
 		
 		$this->db->from('add_class a');
 		$this->db->join('assignment_checker b', 'b.add_class_id=a.add_class_id');
